e5dc8982590dd880034a49f71201c8d6a0701f2d
taylorreiter <taylorreiter@gmail.com>
2019-11-09 10:58:25 -0800

    fix parsing r1 r2 error

--- scripts/parse_md5checksums.R
+++ scripts/parse_md5checksums.R
@@ -22,9 +22,6 @@ table(prj$study_accession)
 # pediatric crohn's data set: https://raw.githubusercontent.com/louiejtaylor/sbx_lewis2015/master/metadata/metadata_with_SRR.csv
 # PRJNA400072 matches library name
 
-
-
-
 md5 <- select(prj, fastq_ftp,  fastq_md5)
 
 
@@ -56,9 +53,9 @@ for(i in 1:length(md5$fastq_ftp)){
   }
 }
 
-md5$fastq_ftp_1 <- fastq_1
-md5$fastq_ftp_2 <- fastq_2
-md5 <- select(md5, fastq_ftp_1, fastq_ftp_2, fastq_md5)
+md5$fastq_final_ftp1 <- fastq_1
+md5$fastq_final_ftp2 <- fastq_2
+md5 <- select(md5, fastq_ftp_1, fastq_final_ftp1, fastq_final_ftp2, fastq_md5)
 
 
 # separate md5sums too ----------------------------------------------------
@@ -71,7 +68,7 @@ md5 <- separate(data = md5,
 
 md5_1 <- vector()
 for(i in 1:length(md5$md5_1)){
-  if(grepl(pattern = "_1", x = md5$md5_1[i])){
+  if(grepl(pattern = "_1", x = md5$fastq_ftp_1[i])){
     md5_1[i] <- md5$md5_1[i] 
   } else {
     md5_1[i] <- md5$md5_2[i]
@@ -80,7 +77,7 @@ for(i in 1:length(md5$md5_1)){
 
 md5_2 <- vector()
 for(i in 1:length(md5$md5_1)){
-  if(grepl(pattern = "_1", x = md5$md5_1[i])){
+  if(grepl(pattern = "_1", x = md5$fastq_ftp_1[i])){
     md5_2[i] <- md5$md5_2[i] 
   } else {
     md5_2[i] <- md5$md5_3[i]
@@ -93,23 +90,24 @@ md5$md5_2 <- md5_2
 
 # make forward and reverse ------------------------------------------------
 
-md5_forward <- select(md5, md5_1, fastq_ftp_1)
-md5_forward$fastq_ftp_1 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/", 
-                                "", md5_forward$fastq_ftp_1)
-md5_forward$fastq_ftp_1 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/",
-                                "", md5_forward$fastq_ftp_1)
+md5_forward <- select(md5, md5_1, fastq_final_ftp1)
+md5_forward$fastq_final_ftp1 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/", 
+                                "", md5_forward$fastq_final_ftp1)
+md5_forward$fastq_final_ftp1 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/",
+                                "", md5_forward$fastq_final_ftp1)
 colnames(md5_forward) <- c('md5sum', "file")
 
 
-md5_reverse <- select(md5, md5_2, fastq_ftp_2)
-md5_reverse$fastq_ftp_2 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/", 
-                                "", md5_reverse$fastq_ftp_2)
-md5_reverse$fastq_ftp_2 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/",
-                                "", md5_reverse$fastq_ftp_2)
+md5_reverse <- select(md5, md5_2, fastq_final_ftp2)
+md5_reverse$fastq_final_ftp2 <- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/", 
+                                "", md5_reverse$fastq_final_ftp2)
+md5_reverse$fastq_final_ftp2<- gsub("ftp.sra.ebi.ac.uk\\/vol1\\/fastq\\/[SERR]{2,3}[0-9]{2,3}\\/[0-9]{2,3}\\/[SERR]{2,3}[0-9]{3,7}\\/",
+                                "", md5_reverse$fastq_final_ftp2)
 
 colnames(md5_reverse) <- c('md5sum', "file")
 
 md5 <- rbind(md5_forward, md5_reverse)
 
-write_tsv(x = md5, path = "inputs/md5checksums.txt", col_names = F)
+write.table(x = md5, file = "inputs/md5checksums.txt", row.names = F, 
+            quote = F, col.names = F, sep = "  ")
 
