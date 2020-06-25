fdb677c5061ffe6630963390cbaca39aa2c3934a
Taylor Reiter <taylorreiter@gmail.com>
2020-06-25 11:46:12 -0700

    grab aas

--- scripts/remove-stop-plass.py
+++ scripts/remove-stop-plass.py
@@ -5,7 +5,7 @@ import os
 
 for filename in sys.argv[1:]:
     print(filename)
-    with open(filename + '.nostop.fa', 'wt') as fp:
+    with open(filename + '.nostop.faa', 'wt') as fp:
         for record in screed.open(filename):
             record.sequence = record.sequence.replace('*', '')
             fp.write('>{}\n{}\n'.format(record.name, record.sequence))
