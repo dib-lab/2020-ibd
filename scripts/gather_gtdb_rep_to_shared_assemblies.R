e167bd8e7cd6a5e850f8757348a66fd2fe7db1ad
taylorreiter <taylorreiter@gmail.com>
2021-06-08 14:08:07 -0700

    update genomes >1%

--- scripts/gather_gtdb_rep_to_shared_assemblies.R
+++ scripts/gather_gtdb_rep_to_shared_assemblies.R
@@ -7,11 +7,12 @@ library(purrr)
 # subset to assemblies identified in all models. 
 # summarize to species level to group queries of the same species together.
 
-# the two output of this script are
-# 1. representative assemblies summarized to lineage
-# 2. representative assemblies as a dummy gather csv output file, to be used
-#    as input to genome-grist. Genome-grist will orchestrate the download of 
-#    these accessions and generate spacegraphcats configuration files. 
+# the output of this script is the representative assemblies as a dummy 
+# gather csv output file, to be used as input to genome-grist. Genome-grist will 
+# orchestrate the download of these accessions and generate spacegraphcats 
+# configuration files. 
+# All shared assemblies are output as one csv file, but the assemblies that
+# hold >1% of variable importance are the ones will be downloaded by genome-grist
 
 # read in gather results and subset to accessions shared between all models
 gather_files <- snakemake@input[["gather"]]
@@ -38,7 +39,7 @@ gather_results_shared_assemblies <- gather_results %>%
 # use an arbitrary gather result file, filter to the shared assemblies, 
 # and output the file as dummy input to genome-grist download.
 
-gather_dummy <- gather_files %>%
+gather_dummy_all <- gather_files %>%
   map_dfr(read_csv)  %>%
   filter(name %in% gather_results_shared_assemblies$name) %>%
   group_by(name) %>%
@@ -50,4 +51,58 @@ gather_dummy <- gather_files %>%
 
 # select Eukaryotic representative species for 
 
-write_csv(gather_dummy, snakemake@outputs[["gather_grist"]])
+write_csv(gather_dummy_all, snakemake@outputs[["gather_all_s"]])
+
+
+# filter based on variable importance -------------------------------------
+
+# shared assemblies are present in all models; read in smallest set
+# reads in signatures of matches and generates a hash:assembly dataframe
+# sig_json <- fromJSON(file = "outputs/gather/SRP057027_vita_vars_gtdb_seed6.matches")
+sig_json <- fromJSON(file = unlist(snakemake@input[["gather"]], use.names = F)[1])
+hash_to_assembly <- lapply(sig_json, data.frame, stringsAsFactors = FALSE)
+hash_to_assembly <- bind_rows(hash_to_assembly)
+
+hash_to_assembly_shared_assemblies <- hash_to_assembly %>%
+  filter(name %in% shared_assemblies$name) %>%
+  select(name, signatures.mins)
+
+# read in and normalize importances from models 
+read_varimp <- function(path_optimal_rf){
+  study <- gsub("outputs\\/optimal_rf_seed\\/", "", path_optimal_rf)
+  study <- gsub("_optimal_rf", "", study)
+  study <- gsub(".RDS", "", study)
+  optimal_rf <- readRDS(path_optimal_rf)
+  varimp <- data.frame(hash = names(optimal_rf$variable.importance), 
+                       importance = optimal_rf$variable.importance,
+                       study = study)
+  varimp <- separate(varimp, col = study, into = c("study", "seed"), sep = "_")
+  rownames(varimp) <- seq(1:nrow(varimp))
+  varimp$hash <- as.numeric(varimp$hash)
+  # add a column where varimp is normalized by the total var imp
+  # e.g., divide by the sum of all variable importances
+  # this will make all variable importance measures sum to 1
+  varimp$model_norm_imp <- varimp$importance / sum(varimp$importance)
+  return(varimp)
+}
+
+#varimp <- Sys.glob("outputs/optimal_rf_seed/*RDS") %>%
+varimp <- unlist(snakemake@input[["varimp"]], use.names = F) %>%
+  map_dfr(read_varimp) %>%
+  mutate(all_model_norm_imp = model_norm_imp / 36)
+
+# join with assembly information 
+varimp <- left_join(varimp, hash_to_assembly_shared_assemblies, by = c("hash" ="signatures.mins"))
+varimp <- varimp %>%
+  mutate(accession = gsub("\\..*", "", name))
+# retain only assemblies that have > 1% of variable importance
+top_imp_genomes <- name_imp %>%
+  filter(total_all_model_norm_imp >= 0.01) 
+
+# this is sort of cheating, but remove the assemblies that I know become <1%
+# after running charcoal to remove contamination
+gather_dummy_top <- gather_dummy_all %>%
+  filter(name %in% top_imp_genomes$name)
+  filter(! name %in% c("GCA_002893375.1 Cyclospora cayetanensis strain=CDC:HCVA02:15, ASM289337v1"))
+
+write_csv(gather_dummy_top, snakemake@outputs[["gather_grist"]])
