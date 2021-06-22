ed6088ca4e7efda5b9cb48e5e6ea5205ad4898e1
taylorreiter <taylorreiter@gmail.com>
2021-06-22 14:31:50 -0700

    fix repeat pieces

--- scripts/cdbg_to_pieces.py
+++ scripts/cdbg_to_pieces.py
@@ -20,19 +20,10 @@ def main():
     output = os.path.join(args.catlas_prefix, output)
     
     catlas = CAtlas(args.cdbg_prefix, args.catlas_prefix)
-    cdbg_to_pieces = defaultdict(set)
-    for node_id in catlas:
-        level = catlas.levels[node_id]
-        if level == 1:
-            pieces = catlas.layer1_to_cdbg.get(node_id)
-            for cdbg_node in pieces:
-                cdbg_to_pieces[cdbg_node] = set(pieces)
-
-    cdbg_to_pieces_df = pd.DataFrame([{"dominator":k, "cdbg_node": v} for k,v in cdbg_to_pieces.items()])
-    # make dict lines one per line
-    cdbg_to_pieces_df = cdbg_to_pieces_df.explode('cdbg_node') 
+    layer1_to_cdbg_df = pd.DataFrame([{"dominator":k, "cdbg_node": v} for k,v in catlas.layer1_to_cdbg.items()])
+    layer1_to_cdbg_df = layer1_to_cdbg_df.explode('cdbg_node') 
     # write to csv
-    cdbg_to_pieces_df.to_csv(output, index = False)
+    layer1_to_cdbg_df.to_csv(output, index = False)
 
     return 0
     
