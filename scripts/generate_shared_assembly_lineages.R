4d7dfdcab35bfd3d75084889d064d6cd28ee226d
taylorreiter <taylorreiter@gmail.com>
2021-07-08 09:43:37 -0700

    make genome as ident instead of acc

--- scripts/generate_shared_assembly_lineages.R
+++ scripts/generate_shared_assembly_lineages.R
@@ -3,14 +3,14 @@ library(readr)
 library(tidyr)
 
 # read in gather csv for shared assemblies and db lineages.
-# join together and output. 
+# join together, format accession to filename, and output. 
 # required input for charcoal decontamination
 
 gather_shared_assemblies <- read_csv(snakemake@input[["gather"]]) %>%
   select(name) %>%
   separate(name, into = "ident", sep = " ")
 db_lineages <- read_csv(snakemake@input[["db_lineages"]])
-gather_shared_assemblies_lineages <- left_join(gather_shared_assemblies, db_lineages, by = "ident")
-
+gather_shared_assemblies_lineages <- left_join(gather_shared_assemblies, db_lineages, by = "ident") %>%
+  mutate(ident = paste0(ident, "_genomic.fna.gz"))
 write_csv(gather_shared_assemblies_lineages, snakemake@output[["lineages"]], 
           col_names = F)
