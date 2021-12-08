d12b7fac7a74b188e20cdf8e5770a1ba6a8c6ea8
taylorreiter <taylorreiter@gmail.com>
2021-12-08 14:09:58 -0800

    typos

--- scripts/grab_differentially_abundant_dom_ids.R
+++ scripts/grab_differentially_abundant_dom_ids.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(readr)
 
 #sig_ccs <- read_tsv("outputs/sgc_pangenome_catlases_corncob/GCA_000210075.1_sig_ccs.tsv") %>%
-sig_ccs <- read_tsv(snakemake@input[[sig_ccs]]) %>%
+sig_ccs <- read_tsv(snakemake@input[['sig_ccs']]) %>%
   select(mu, estimate, dom_id = aa_seq) %>%
   mutate(mu = gsub("mu.diagnosis", "", mu)) %>%
   mutate(abundance = ifelse(estimate > 0, "increased", "decreased"))
@@ -13,7 +13,7 @@ sig_ccs %>%
   group_by(mu, abundance) %>%
   #group_walk(~ write_tsv(.x, paste0(.y$mu, "_", .y$abundance, "_nbhds.gz"), col_names = F))
   group_walk(~ write_tsv(.x, paste0(snakemake@params[['outdir']], 
-                                    snakemake@wildcard[['acc']], "_",
+                                    snakemake@wildcards[['acc']], "_",
                                     .y$mu, "_", .y$abundance, "_dom_ids.tsv.gz"), 
                          col_names = F))
 
