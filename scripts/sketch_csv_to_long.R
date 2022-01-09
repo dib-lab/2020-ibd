bddbde33afde8992dc9de9e40ba89752b153dfea
taylorreiter <taylorreiter@gmail.com>
2022-01-08 19:52:13 -0800

    proper naming acc, if csv empty

--- scripts/sketch_csv_to_long.R
+++ scripts/sketch_csv_to_long.R
@@ -2,20 +2,23 @@
 files <- snakemake@input
 files <- unlist(files, use.names=FALSE)
 
-## read files into list with each row labelled by accession. 
 tab_long <- list()
 for(i in 1:length(files)){
   print(i)
   sig <- read.csv(files[i])           # read in signature csv as df
-  acc <- colnames(sig)[1]             # set lib name using sample
-  acc <- gsub("*.sig", "", acc)       # remove file suffix
-  sig$acc <- acc                      # set libname as col
-  colnames(sig) <- c("minhash", "acc")
-  tab_long[[i]] <- sig
+  acc <- colnames(sig)[2]             # set lib name using sample
+  acc <- gsub(".sig", "", acc)        # remove file suffix
+  acc <- gsub("^X", "", acc)          # remove prepended X
+  if(nrow(sig) > 0){
+    sig$acc <- acc                      # set libname as col
+    colnames(sig) <- c("minhash", "abund", "acc")
+    tab_long[[i]] <- sig
+  }
 }
 
 ## bind into one dataframe
 tab_long <- do.call(rbind, tab_long)
+tab_long <- tab_long[ , c("minhash", "acc")]
 tab_long$present <- 1 # add a col with "1" for presence/absence
 write.csv(tab_long, snakemake@output[['csv']],
           quote = F, row.names = F)
